95ee3db1e11839df820ad4f700695b4a2a6ca0b2
Nuno Ferreira <nuno.ferreira@corepower.energy>
2024-08-16 18:47:08 +0100

    Modifed rename button to run the rename_files script

--- src/script.py
+++ src/script.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-def rename_files(directory: Path, new_name: str) -> None:
+def rename_files(directory: str, new_name: str) -> None:
     """Rename the files in the directory."""
     print(f"Renaming files in {directory} to {new_name}")
     pass
